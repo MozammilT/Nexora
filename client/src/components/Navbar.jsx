d40830c871772e71f5d6dfe8a16720c708ef4c78
Mozammil <mozammiltarique007@gmail.com>
2025-07-23 19:31:16 +0530

    Improve Navbar and add Clerk Authentication

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,32 @@
 import { useTheme } from "../context/ThemeContext.jsx";
+import { useNavigate } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 function Navbar() {
   const { theme, toggleTheme } = useTheme();
   const darkMode = theme === "dark";
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
 
   return (
     <nav
-      class={`h-[70px] relative w-full px-6 md:px-16 lg:px-24 xl:px-32 flex items-center justify-between z-20 text-gray-700 shadow-[0px_4px_25px_0px_#0000000D] transition-all ${
-        darkMode ? "bg-[#121212]" : "bg-white"
+      className={`h-[70px] relative w-full px-6 max-sm:pl-2 md:px-16 lg:px-24 xl:px-32 flex items-center justify-between z-20 text-gray-700 shadow-[0px_4px_25px_0px_#0000000D] transition-all ${
+        darkMode ? "bg-[#000000]" : "bg-white"
       }`}
     >
-      <a href="https://prebuiltui.com" class="text-indigo-600">
-        <img src="/logo3_light_crop.png" className="h-18 w-50" />
-      </a>
-      <div className="flex justify-center items-center gap-10">
+      <img
+        src={`${darkMode ? "/logo3_dark_crop.png" : "/logo3_light_crop.png"}`}
+        className="h-12 sm:h-16 w-auto cursor-pointer"
+        onClick={() => navigate("/")}
+      />
+      <div className="flex justify-center items-center gap-10 max-sm:gap-4">
         <button
-          class="size-8 flex items-center justify-center hover:bg-gray-100 transition border border-slate-300 rounded-md"
+          className={`size-8 flex items-center justify-center transition border rounded-md cursor-pointer ${
+            darkMode
+              ? "bg-gray-800 border-gray-600 hover:bg-gray-700"
+              : "bg-white border-gray-300 hover:bg-gray-100"
+          }`}
           onClick={toggleTheme}
         >
           <svg
@@ -24,47 +35,28 @@ function Navbar() {
             viewBox="0 0 15 15"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            className={`${darkMode ? "stroke-white" : "stroke-gray-800"}`}
           >
             <path
               d="M7.5 10.39a2.889 2.889 0 1 0 0-5.779 2.889 2.889 0 0 0 0 5.778M7.5 1v.722m0 11.556V14M1 7.5h.722m11.556 0h.723m-1.904-4.596-.511.51m-8.172 8.171-.51.511m-.001-9.192.51.51m8.173 8.171.51.511"
-              stroke="#353535"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </svg>
         </button>
-        <button
-          type="button"
-          class="bg-primary text-white border md:inline hidden text-sm hover:bg-[#0369a1] active:scale-95 transition-all w-40 h-11 rounded-full"
-        >
-          Get started
-        </button>
-      </div>
 
-      <button
-        aria-label="menu-btn"
-        type="button"
-        class="menu-btn inline-block md:hidden active:scale-90 transition"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          viewBox="0 0 30 30"
-          fill="#000"
-        >
-          <path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"></path>
-        </svg>
-      </button>
-
-      <div class="mobile-menu absolute top-[70px] left-0 w-full bg-white p-6 hidden md:hidden">
-        <button
-          type="button"
-          class="bg-primary text-gray-600 border border-gray-300 mt-6 text-sm hover:bg-gray-50 active:scale-95 transition-all w-40 h-11 rounded-full"
-        >
-          Get started
-        </button>
+        {user ? (
+          <UserButton />
+        ) : (
+          <button
+            type="button"
+            onClick={openSignIn}
+            className="bg-primary text-white md:inline text-sm hover:bg-[#0369a1] active:scale-95 transition-all w-40 max-sm:w-25 h-11 max-sm:h-8 rounded-full cursor-pointer"
+          >
+            Get started
+          </button>
+        )}
       </div>
     </nav>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,20 @@ import "./styles/index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </ClerkProvider>
 );
