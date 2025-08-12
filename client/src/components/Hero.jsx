a6438e156c775b7517e9eef794af8b74f191d379
Mozammil <mozammiltarique007@gmail.com>
2025-08-12 23:51:17 +0530

    Improved UI on smaller screens

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,13 +1,9 @@
 "use client";
-import { useTheme } from "../context/ThemeContext.jsx";
 import { motion } from "motion/react";
 import { HeroHighlight, Highlight } from "./ui/hero-highlight.jsx";
 import { PrimaryButton, SecondaryButton } from "./Button.jsx";
 
 function Hero() {
-  const { theme } = useTheme();
-  const darkMode = theme === "dark";
-
   return (
     <HeroHighlight>
       <motion.h1
@@ -23,16 +19,12 @@ function Hero() {
           duration: 0.5,
           ease: [0.4, 0.0, 0.2, 1],
         }}
-        className={`text-2xl px-4 md:text-4xl lg:text-7xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto md:mt-20`}
+        className={`text-4xl px-4 lg:text-7xl font-bold text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto md:mt-20`}
       >
         Create amazing content <br /> with
-        <Highlight
-          className={`${darkMode ? "text-white" : "text-black"} ml-2 md:ml-4`}
-        >
-          AI tools
-        </Highlight>
+        <Highlight className="text-white ml-2 md:ml-4">AI tools</Highlight>
       </motion.h1>
-      <p className="text-white mt-4 max-w-xs sm:max-w-sm xl:max-w-2xl m-auto max-sm:text-xs text-xl text-center font-semibold">
+      <p className="text-white mt-4 max-w-xs sm:max-w-sm xl:max-w-2xl m-auto max-sm:text-sm text-xl text-center font-semibold">
         Transform your content creation with our suite of premium AI tools.
         Write articles, generate images, and enhance your workflow.
       </p>
